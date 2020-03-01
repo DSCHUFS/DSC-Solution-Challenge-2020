f1fb8355178f8f38bafc103d0d376d46cb061c8e
hankyul <rlagksruf16@gmail.com>
2020-03-01 14:46:20 +0900

    create pages

---
+++ lib/mainPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MainPage extends StatefulWidget {
+  
+  static const String id = 'main_page';
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
