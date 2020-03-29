4402fdb6186bc9d85ca7916e19efee05ef75af57
hankyul <rlagksruf16@gmail.com>
2020-03-29 20:50:00 +0900

    add report list

---
+++ lib/models/report.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Report{
+
+  final String name;
+  final DateTime writedate;
+  final DateTime visitdate;
+  final 
+
+
+  Report({
+    this.name,
+    this.writedate,
+    this.visitdate,
+    
+  })
+
+
+}
